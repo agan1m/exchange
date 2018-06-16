7728a21f438ebdbfefeedaebba5ed08441726506
agan1m <marsov-v@mail.ru>
2018-06-16 16:33:02 +0300

    init

---
+++ src/components/AppRouter/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class AppRouter extends Component {
+    
+    render() {
+        return (
+            <h1>eee</h1>
+        );
+    }
+}
+
+export default AppRouter;
---
+++ src/components/PrivateRoute/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class PrivateRoute extends Component {
+    
+    render() {
+        return (
+            <h1>111</h1>
+        );
+    }
+}
+
+export default PrivateRoute;
